6e11f2b7f848dff3430d58d71a1e10e9af63bb18
zero <tobewhatwewant@gmail.com>
2019-01-10 18:24:58 +0800

    :white_check_mark:(jest): migrate mocha to jest

--- test/index.spec.ts
+++ test/index.spec.ts
@@ -1,4 +1,3 @@
-import { expect } from 'chai';
 import { delay as sleep } from '@zcorky/delay';
 import { lru as LRU } from '../src';
 
@@ -11,12 +10,12 @@ describe('Lru tests', () => {
   describe('create instance', () => {
     it('using new', () => {
       const lru = new LRU<string, number>(10);
-      expect(lru instanceof LRU).to.equal(true);
+      expect(lru instanceof LRU).toEqual(true);
     });
 
     it('using create', () => {
       const lru = LRU.create<string, number>(10);
-      expect(lru instanceof LRU).to.equal(true);
+      expect(lru instanceof LRU).toEqual(true);
     });
   });
 
@@ -24,40 +23,40 @@ describe('Lru tests', () => {
     it('key is string', () => {
       const lru = new LRU<string, string>(10);
       lru.set('foo', 'bar');
-      expect(lru.get('foo')).to.equal('bar');
+      expect(lru.get('foo')).toEqual('bar');
     });
 
     it('key is number', () => {
       const lru = new LRU<number, string>(10);
       lru.set(1, 'bar');
-      expect(lru.get(1)).to.equal('bar');
+      expect(lru.get(1)).toEqual('bar');
     });
 
     it('key is object', () => {
       const lru = new LRU<object, string>(10);
       const obj = { hi: 'foo' };
       lru.set(obj, 'bar');
-      expect(lru.get(obj)).to.equal('bar');
+      expect(lru.get(obj)).toEqual('bar');
     });
 
     it('value is empty value', () => {
       const lru = new LRU<string, any>(10);
       lru.set('foo', 'bar');
-      expect(lru.get('foo')).to.equal('bar');
+      expect(lru.get('foo')).toEqual('bar');
       lru.set('foo', null);
-      expect(lru.get('foo')).to.equal(null);
+      expect(lru.get('foo')).toEqual(null);
       lru.set('foo', undefined);
-      expect(lru.get('foo')).to.equal(undefined);
+      expect(lru.get('foo')).toEqual(undefined);
       lru.set('foo', 0);
-      expect(lru.get('foo')).to.equal(0);
+      expect(lru.get('foo')).toEqual(0);
       lru.set('foo', '');
-      expect(lru.get('foo')).to.equal('');
+      expect(lru.get('foo')).toEqual('');
     });
 
     it('value is object', () => {
       const lru = new LRU<string, { foo: string }>(10);
       lru.set('foo', { foo: 'bar' });
-      expect(lru.get('foo')!.foo).to.equal('bar');
+      expect(lru.get('foo')!.foo).toEqual('bar');
     });
 
     it('expired value should not copy', async () => {
@@ -65,29 +64,29 @@ describe('Lru tests', () => {
       lru.set('foo1', 'bar');
       lru.set('foo', 'bar', { maxAge: 2 });
 
-      expect((lru as any).cache.size).to.equal(0);
-      expect((lru as any)._cache.size).to.equal(2);
-      // expect(lru.get('foo1', { maxAge: 2 })).to.equal('bar');
+      expect((lru as any).cache.size).toEqual(0);
+      expect((lru as any)._cache.size).toEqual(2);
+      // expect(lru.get('foo1', { maxAge: 2 })).toEqual('bar');
 
       await sleep(10);
-      expect(lru.get('foo')).to.equal(undefined);
-      expect(lru.get('foo1')).to.equal('bar');
-      expect((lru as any).cache.size).to.equal(1);
-      expect((lru as any)._cache.size).to.equal(2);
+      expect(lru.get('foo')).toEqual(undefined);
+      expect(lru.get('foo1')).toEqual('bar');
+      expect((lru as any).cache.size).toEqual(1);
+      expect((lru as any)._cache.size).toEqual(2);
 
       await sleep(10);
-      expect(lru.get('foo')).to.equal(undefined);
-      expect(lru.get('foo1')).to.equal('bar');
-      expect((lru as any).cache.size).to.equal(1);
-      expect((lru as any)._cache.size).to.equal(2);
+      expect(lru.get('foo')).toEqual(undefined);
+      expect(lru.get('foo1')).toEqual('bar');
+      expect((lru as any).cache.size).toEqual(1);
+      expect((lru as any)._cache.size).toEqual(2);
 
       lru.set('foo2', 'bar');
-      expect((lru as any).cache.size).to.equal(0);
-      expect((lru as any)._cache.size).to.equal(2);
-      expect(lru.get('foo')).to.equal(undefined);
-      expect(lru.get('foo2')).to.equal('bar');
-      expect((lru as any).cache.size).to.equal(1);
-      expect((lru as any)._cache.size).to.equal(2);
+      expect((lru as any).cache.size).toEqual(0);
+      expect((lru as any)._cache.size).toEqual(2);
+      expect(lru.get('foo')).toEqual(undefined);
+      expect(lru.get('foo2')).toEqual('bar');
+      expect((lru as any).cache.size).toEqual(1);
+      expect((lru as any)._cache.size).toEqual(2);
     });
   });
 
@@ -99,14 +98,14 @@ describe('Lru tests', () => {
         lru.set('k2', 'v2', { maxAge });
         lru.set('k3', { foo: 'bar' }, { maxAge });
 
-        expect(lru.get(1)).to.equal(0);
-        expect(lru.get('k2')).to.equal('v2');
-        expect((lru.get('k3') as any).foo).to.equal('bar');
+        expect(lru.get(1)).toEqual(0);
+        expect(lru.get('k2')).toEqual('v2');
+        expect((lru.get('k3') as any).foo).toEqual('bar');
 
         await sleep(maxAge + 10);
-        expect(lru.get(1)).to.equal(undefined);
-        expect(lru.get('k2')).to.equal(undefined);
-        expect(lru.get('k3')).to.equal(undefined);
+        expect(lru.get(1)).toEqual(undefined);
+        expect(lru.get('k2')).toEqual(undefined);
+        expect(lru.get('k3')).toEqual(undefined);
       });
     });
   });
@@ -119,22 +118,22 @@ describe('Lru tests', () => {
         lru.set('k2', 'v2', { maxAge });
         lru.set('k3', { foo: 'bar' }, { maxAge });
 
-        expect(lru.get(1)).to.equal(0);
-        expect(lru.get('k2')).to.equal('v2');
-        expect((lru.get('k3') as any).foo).to.equal('bar');
+        expect(lru.get(1)).toEqual(0);
+        expect(lru.get('k2')).toEqual('v2');
+        expect((lru.get('k3') as any).foo).toEqual('bar');
 
         await sleep(maxAge - 10);
-        expect(lru.get(1, { maxAge })).to.not.equal(undefined);
-        expect(lru.get('k2', { maxAge })).to.not.equal(undefined);
-        expect(lru.get('k3', { maxAge })).to.not.equal(undefined);
+        expect(lru.get(1, { maxAge })).not.toBe(undefined);
+        expect(lru.get('k2', { maxAge })).not.toBe(undefined);
+        expect(lru.get('k3', { maxAge })).not.toBe(undefined);
 
         await sleep(maxAge - 10);
-        expect(lru.get(1, { maxAge })).to.not.equal(undefined);
-        expect(lru.get('k2', { maxAge })).to.not.equal(undefined);
-        expect(lru.get('k3', { maxAge })).to.not.equal(undefined);
-        expect(lru.get(1, { maxAge })).to.not.equal(undefined);
-        expect(lru.get('k2', { maxAge })).to.not.equal(undefined);
-        expect(lru.get('k3', { maxAge })).to.not.equal(undefined);
+        expect(lru.get(1, { maxAge })).not.toBe(undefined);
+        expect(lru.get('k2', { maxAge })).not.toBe(undefined);
+        expect(lru.get('k3', { maxAge })).not.toBe(undefined);
+        expect(lru.get(1, { maxAge })).not.toBe(undefined);
+        expect(lru.get('k2', { maxAge })).not.toBe(undefined);
+        expect(lru.get('k3', { maxAge })).not.toBe(undefined);
       });
     });
 
@@ -145,14 +144,14 @@ describe('Lru tests', () => {
       lru.get('foo1', { maxAge: 100 });
 
       await sleep(50);
-      expect(lru.get('foo1')).to.equal('bar');
-      expect(lru.get('foo2', { maxAge: 0 })).to.equal('bar');
-      expect(lru.get('foo2', { maxAge: 0 })).to.equal('bar');
-      expect(lru.get('foo2', { maxAge: 0 })).to.equal('bar');
+      expect(lru.get('foo1')).toEqual('bar');
+      expect(lru.get('foo2', { maxAge: 0 })).toEqual('bar');
+      expect(lru.get('foo2', { maxAge: 0 })).toEqual('bar');
+      expect(lru.get('foo2', { maxAge: 0 })).toEqual('bar');
 
       await sleep(120);
-      expect(lru.get('foo')).to.equal(undefined);
-      expect(lru.get('foo2')).to.equal('bar');
+      expect(lru.get('foo')).toEqual(undefined);
+      expect(lru.get('foo2')).toEqual('bar');
     });
   });
 })
